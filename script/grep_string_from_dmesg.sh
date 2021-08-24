a170607a8717632f98f31b3c7461f3305eb99547
Qiyuan Pu <pqy7172@gmail.com>
2021-08-24 20:41:46 +0800

    add a script to grep dmesg

---
+++ script/grep_string_from_dmesg.sh
@@ -0,0 +1,12 @@
+#usage: ./grep_string_from_dmesg.sh "string you want to grep like this"
+#!/bin/bash
+
+echo string you want to grep: $1
+while true; do
+	res=`dmesg |grep -e "$1"`
+	if [[ ! -z "$res" ]]
+	then
+		dmesg > log.txt
+		exit
+	fi
+done
