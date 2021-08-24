caab66091f84f79366c7237e6dce35cdd0b2dd6c
Qiyuan Pu <pqy7172@gmail.com>
2021-08-24 20:45:58 +0800

    running grep script in background

--- script/grep_string_from_dmesg.sh
+++ script/grep_string_from_dmesg.sh
@@ -1,4 +1,4 @@
-#usage: ./grep_string_from_dmesg.sh "string you want to grep like this"
+#usage: ./grep_string_from_dmesg.sh "string you want to grep like this" &
 #!/bin/bash
 
 echo string you want to grep: $1
