da773cf70bf9f94b476b0eb46f19b3faf4fda947
Qiyuan Pu <pqy7172@gmail.com>
2021-09-02 17:32:39 +0800

    add how to execute command on all machine

---
+++ script/all_machine_do_one_command.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo $1 #remote machine info
+echo $2 #the command string you want to execute
+
+while IFS= read -r line
+do
+        username=`echo $line |awk '{print $1}'`
+        ip=`echo $line |awk '{print $2}'`
+        passwd=`echo $line |awk '{print $3}'`
+
+		./do_one_command $username $ip $passwd "$2"
+done < "$1"
