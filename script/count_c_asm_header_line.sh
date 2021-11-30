5574df0333e80d2c62cc3cab9f3b6024a4da4c35
Qiyuan Pu <pqy7172@gmail.com>
2021-11-30 14:35:50 +0800

    add count program line

---
+++ script/count_c_asm_header_line.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# echo $1 source dir
+
+tot_line=0
+for f in $(find $1 -name '*.c' -or -name '*.S' -or -name '*.h' -or -name '*.s');
+do
+	current_lines=`cat $f |wc -l`
+	((tot_line+=$current_lines))
+done
+
+echo all lines: $tot_line
