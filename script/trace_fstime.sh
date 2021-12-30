4d64c8a0d01e47d8b3261e5232069c79cc6f529f
Qiyuan Pu <pqy7172@gmail.com>
2021-12-30 19:13:23 +0800

    update for cat proc sched file

--- script/trace_fstime.sh
+++ script/trace_fstime.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 cnt=1
-while [ $cnt -le 3 ]
+while [ $cnt -le 1 ]
 do
     pids=`ps aux |grep fstime |grep 30 |grep -v sh |grep -v perl |grep -v grep |awk '{print $2}'`
     if [  -z "$pids" ]
     then
 	continue
     else
+	sleep 25
 	for pid in $pids
 	do
-	    mkdir $pid
-	    cd $pid
-	    trace-cmd record -P $pid -p function_graph &
-	    cd ..
+	    #mkdir $pid
+	    #cd $pid
+	    #trace-cmd record -P $pid -p function_graph &
+	    #cd ..
+	    echo $pid
+	    cat /proc/$pid/sched
+	    echo
 	done
-	sleep 30
 	((cnt++))
     fi
+    pids=
 done
