17f3b67df5acfe8686718c1720edd3e36f693f55
Qiyuan Pu <pqy7172@gmail.com>
2022-09-06 22:13:17 +0800

    count cc file line number

--- script/count_c_asm_header_line.sh
+++ script/count_c_asm_header_line.sh
@@ -3,7 +3,7 @@
 # echo $1 source dir
 
 tot_line=0
-for f in $(find $1 -name '*.c' -or -name '*.S' -or -name '*.cc' -or -name '*.h' -or -name '*.s');
+for f in $(find $1 -name '*.c' -or -name '*.S' -or -name '*.cpp' -or -name '*.cc' -or -name '*.h' -or -name '*.s');
 do
 	current_lines=`cat $f |wc -l`
 	((tot_line+=$current_lines))
