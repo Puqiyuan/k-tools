ff4c5ecd49744115a449d3ce6501f513553763b7
Qiyuan Pu <pqy7172@gmail.com>
2024-04-03 15:50:05 +0800

    add kprobe simple example

---
+++ es-diagnose/kprobe/kprobe_example/kprobe_module.c
@@ -0,0 +1,60 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/kprobes.h>
+
+static struct kprobe kp;
+
+/* kprobe pre_handler: called just before the probed instruction is executed */
+int handler_pre(struct kprobe *p, struct pt_regs *regs)
+{
+    printk(KERN_INFO "Hello\n");
+    return 0;
+}
+
+/* kprobe post_handler: called after the probed instruction is executed */
+void handler_post(struct kprobe *p, struct pt_regs *regs, unsigned long flags)
+{
+    /* Nothing here */
+}
+
+/* kprobe fault_handler: called if an exception occurred in the probed instruction */
+int handler_fault(struct kprobe *p, struct pt_regs *regs, int trapnr)
+{
+    printk(KERN_INFO "fault_handler: Detected trap %d, errno %ld\n", trapnr, regs->si);
+    return 0;
+}
+
+static int __init kprobe_init(void)
+{
+    int ret;
+
+    kp.pre_handler = handler_pre;
+    kp.post_handler = handler_post;
+    kp.fault_handler = handler_fault;
+    kp.addr = (kprobe_opcode_t *)kallsyms_lookup_name("ksys_read");
+	//kp.symbol_name = "sys_read";
+
+	if (!kp.addr) {
+        printk(KERN_INFO "Failed to find address of sys_read\n");
+        return -1;
+    }
+    ret = register_kprobe(&kp);
+    if (ret < 0) {
+        printk(KERN_INFO "Failed to register kprobe\n");
+        return ret;
+    }
+    printk(KERN_INFO "Planted kprobe at %p\n", kp.addr);
+
+    return 0;
+}
+
+static void __exit kprobe_exit(void)
+{
+    unregister_kprobe(&kp);
+    printk(KERN_INFO "kprobe at %p unregistered\n", kp.addr);
+}
+
+module_init(kprobe_init);
+module_exit(kprobe_exit);
+
+MODULE_LICENSE("GPL");
